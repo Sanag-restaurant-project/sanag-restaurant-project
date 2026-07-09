aba83300353a65ca3176a5bc281bb1f22dea5c46
Fathiya4545 <fathiyayoosef85@gmail.com>
2026-07-08 21:14:12 -0700

    Add complete pages

--- js/script.js
+++ js/script.js
@@ -9,3 +9,28 @@ function toggleMenu() {
    
 
 }
+
+
+
+function loadfoodImage(imageId, meal) {
+
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+    .then(response => response.json())
+    .then(data => {
+     document.getElementById(imageId).src = data.meals[0].strMealThumb;
+
+    })
+    .catch(error => console.log(error));
+
+
+
+}
+
+
+
+loadfoodImage("food1", "Chicken");
+loadfoodImage("food2", "Beef");
+loadfoodImage("food3", "Lamb");
+loadfoodImage("food4", "Rice");
+loadfoodImage("food5", "Basta");
+loadfoodImage("food6" , "Breakfast");
